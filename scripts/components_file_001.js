d55d2ba17c88f435c23f9d705352e2dcb09f6267
coolerputt01 <olumideadekolu9@gmail.com>
2025-06-28 20:38:46 +0100

    Adding Error handling

--- scripts/components_file_001.js
+++ scripts/components_file_001.js
@@ -2,6 +2,7 @@ const main = document.querySelector('#main');
 
 const filePath1 = "../fileComponents/components.nuek";
 const filePath2 = "../fileComponents/components2.nuek"
+const filePath3 = "../fileComponents/components3.nuek"
 
 async function file_data(file){
   if(!file) {
@@ -14,7 +15,14 @@ async function file_data(file){
       throw new Error("\nCouldn't read from *nuek File properly: try checking the component filePath");
     }
     var text = await response.text();
-    
+    const generatedRandInt = Math.floor(Math.random() * 1000)
+    if(!text.includes("<component>")){
+      throw new Error(`Expected a component tag but couldn't see it in '${file}'`)
+    }else if(!text.includes("</component>")){
+      throw new Error(`Expected a closing component tag at '${file}'`)
+    } else if(!text.includes("<component>") && !text.includes("</component>")){
+      throw new Error(`No component tag found at '${file}'`)
+    }
     main.innerHTML += text;
     
   }catch(error) {
@@ -24,3 +32,4 @@ async function file_data(file){
 
   file_data(filePath1);
   file_data(filePath2);
+  file_data(filePath3);
