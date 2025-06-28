a78b0027fd61a88bc2196d67465ec3e333b357b4
coolerputt01 <olumideadekolu9@gmail.com>
2025-06-28 23:55:22 +0100

    update

--- scripts/components_file_001.js
+++ scripts/components_file_001.js
@@ -10,32 +10,41 @@ function generateRandInt() {
   return Math.floor(Math.random() * 10000) + 6352;
 }
 
+function throwError(text,file){
+  if (!text.includes("<component>") || !text.includes("</component>")) {
+  throw new Error(`Missing <component> or </component> tag in '${file}'`);
+}else if(!(text.includes("<style>") && text.includes("@scope"))){
+  throw new Error(`Scope style not found in '${file}'`)
+}else if(text.includes("@scope(component)")){
+  console.warn(`Rewrite this "@scope(component)" with this "@scope (component)"`);
+  let textPos = text.indexOf("@scope");
+  let formattedText = text.slice(0,textPos+ "@scope".length) + ' ' + text.slice(textPos + "@scope".length);
+  return formattedText
+}
+return text
+}
+
 async function file_data(file) {
   if (!file) throw new Error("No component file detected...");
   
   try {
     const response = await fetch(file);
     if (!response.ok)
-      throw new Error(`Couldn't read *.nuek file: ${file}`);
+      throw new Error(`Couldn't read .nuek file: ${file}`);
     
     let text = await response.text();
-    
-    if (!text.includes("<component>") || !text.includes("</component>")) {
-      throw new Error(`Missing <component> or </component> tag in '${file}'`);
-    }
+    text = throwError(text,file);
     
     const gRI = generateRandInt();
-    console.log(gRI);
     
     text = text
       .replaceAll("<component>", `<component-${gRI}>`)
       .replaceAll("</component>", `</component-${gRI}>`)
-      .replace("@scope (component)", `@scope (component-${gRI})`);
+      .replace("@scope (component)", `@scope (component-${gRI})`)
     
-    console.log(text);
     main.innerHTML += text;
   } catch (error) {
-    console.error("Error reading *.nuek component:\n", error.message);
+    console.error("Error reading .nuek component:\n", error.message);
   }
 }
 
