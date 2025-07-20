ae9c05621bcf9407d6388d821b3dabef0e7bd818
coolerputt01 <olumideadekolu9@gmail.com>
2025-07-20 16:22:15 +0100

    Differentiated Static components from Prop-dynamic ones

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import { componentRead } from './utils.js/componentRead.js'
 
-class NuekComponent {
+class NuekStaticComponent {
   constructor(selector 
   = "body",file,props=null,loop=1,condition=true){
     if(typeof selector === "string"){
@@ -8,7 +8,7 @@ class NuekComponent {
     }
     this.selector = selector;
     this.file = file;
-    this.props = props;
+    this.props = props || [];
     this.condition = condition;
     this.loop = loop;
     if(condition === true){
@@ -23,4 +23,31 @@ class NuekComponent {
   }
 }
 
-window.NuekComponent = NuekComponent;
+class NuekPropsComponent {
+  constructor(selector 
+  = "body",file,props=null,condition=true){
+    if(typeof selector === "string"){
+      selector = document.querySelector(selector);
+    }
+    this.selector = selector;
+    this.file = file;
+    this.props = props || [];
+    this.condition = condition;
+    if(condition === true){
+    this.init();
+    console.log("how far")
+    }
+  }
+  init() {
+    if(this.props !== null && Array.isArray(this.props)){
+      for(var i = 0; i < this.props.length;i++){
+        componentRead(this.selector,this.file,this.props[i]);
+      }
+    }else {
+      throw new Error("Props is not a valid type 'Array[]()'");
+    }
+  }
+}
+
+window.NuekStaticComponent = NuekStaticComponent;
+window.NuekPropsComponent = NuekPropsComponent;
