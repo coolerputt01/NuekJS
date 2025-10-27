e1d81d6c4c8da5986177f565909f03344c18a5a8
coolerputt <olumideadekolu9@gmail.com>
2025-10-27 22:47:59 +0000

    Fixing bug

--- ssr/nuekssr/src/main.rs
+++ ssr/nuekssr/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Nuek's SSR!");
+use axum::{Router,routing::post,response::Html,Json};
+use serde::Deserialise;
+use std::net::SocketAddr;
+use tower_http::cors::CorsLayer;
+use tower::ServiceBuilder::layer;
+
+#[derive(Deserialise)]
+struct RenderHTMLAsJSON {
+    html: String,
+}
+
+async fn renderHTML(Json(body) : Json<RenderHTMLAsJSON>) -> Html<String> {
+    Html(format!("{}",body.html));
+}
+#[tokio::main]
+async fn main() {
+    let cors = CorsLayer::very_permissive();
+    let app = Router::new().route("/render",post(renderHTML)).layer(cors);
+    let addr = SocketAddr::from(([127,0,0,1],3000));
+    println!("Listening on port: 3000.\n");
+    axum::Server::bind(&addr).serve(app.into_make_service()).await.unwrap();
+
 }
---
+++ ssr/nuekssr/target/debug/deps/tokio_macros-4425fc48ae901d9b.d
@@ -0,0 +1,7 @@
+/workspaces/NuekJS/ssr/nuekssr/target/debug/deps/tokio_macros-4425fc48ae901d9b.d: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/entry.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/select.rs
+
+/workspaces/NuekJS/ssr/nuekssr/target/debug/deps/libtokio_macros-4425fc48ae901d9b.so: /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/lib.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/entry.rs /home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/select.rs
+
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/lib.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/entry.rs:
+/home/codespace/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/tokio-macros-2.6.0/src/select.rs:
